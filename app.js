3c3c311f0d8faeca1766a6d4a72379826c76b039
Giovanny EL PRO <gamc1199@outlook.com>
2022-10-05 22:36:44 -0500

    This is all the code for today 10-October-2022 bye

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const app = express();
+const logger = require('morgan');
+const connection = require('./data-access-layer/config');
+
+
+//**middlewares**//
+app.use(logger('dev'));
+
+
+app.get('/',(req,res) => {
+    connection.query('SELECT * FROM paciente',(err,data) => {
+        if(err) throw err;
+        console.log(data);
+    });
+    res.send("funciona");
+});
 
 module.exports = app;
